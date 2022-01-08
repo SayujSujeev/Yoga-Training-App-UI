40666d09d0807d7ef88f84474f3f354c54a40903
sanakang0615 <82388712+sanakang0615@users.noreply.github.com>
2022-01-08 20:09:30 +0900

    Revised the podfile

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -4,42 +4,42 @@ import 'package:UnivTodo/models/course.dart';
 import 'package:UnivTodo/models/style.dart';
 
 final _standStyle = Style(
-  imageUrl: 'assets/images/pose2.png',
-  name: 'Standing Style',
-  time: 5,
+  imageUrl: 'assets/images/pf1.png',
+  name: 'User 1',
+  time: 400,
 );
 final _sittingStyle = Style(
-  imageUrl: 'assets/images/pose1.png',
-  name: 'Sitting Style',
-  time: 8,
+  imageUrl: 'assets/images/pf2.png',
+  name: 'User 2',
+  time: 442,
 );
 final _legCrossStyle = Style(
-  imageUrl: 'assets/images/pose3.png',
-  name: 'Leg Cross Style',
-  time: 6,
+  imageUrl: 'assets/images/pf1.png',
+  name: 'User 3',
+  time: 623,
 );
 
 final styles = [_standStyle,_sittingStyle,_legCrossStyle];
 
 final _course1 = Course(
-  imageUrl: 'assets/images/course1.jpg',
-  name: 'Advance Stretchings',
-  time: 45,
-  students: 'Advanced'
+  imageUrl: 'assets/images/cs.jpeg',
+  name: 'System Programming',
+  time: 95,
+  students: 'CS230'
 );
 
 final _course2 = Course(
-    imageUrl: 'assets/images/course2.jpg',
-    name: 'Daily Yoga',
-    time: 30,
-    students: 'Intermediate'
+    imageUrl: 'assets/images/os.jpeg',
+    name: 'Operating Systems',
+    time: 95,
+    students: 'CS350'
 );
 
 final _course3 = Course(
-    imageUrl: 'assets/images/course3.jpg',
-    name: 'Meditation',
-    time: 20,
-    students: 'Beginner'
+    imageUrl: 'assets/images/ca.jpg',
+    name: 'Computer Architecture',
+    time: 95,
+    students: 'CS300'
 );
 
 final List<Course> courses = [_course1,_course3,_course2];
